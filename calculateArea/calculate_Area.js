ed8d89e4f1daa13ee4f9dadac1b1cc577083a14c
Piotr <piotrbojar@gmail.com>
2024-04-01 17:13:04 -0400

    followers_count

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -6,6 +6,10 @@ function calculateArea() {
     width = parseFloat(document.getElementById('width').value);
    
    let area = length * width;
+   document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+
    }
 
-document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+function groceryTracker() {
+    
+}
